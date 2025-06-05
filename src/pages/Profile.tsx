35f48787a69c3603f36b9ac20f66f86529ef76e1
TCK-Team <natalya@thecitykitch.com>
2025-06-05 18:42:17 -0400

    Updated Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -181,7 +181,7 @@ const Profile = () => {
 
   if (isLoading) {
     return (
-      <Box height="100vh" display="flex" alignItems="center" justifyContent="center">
+      <Box height="100vh\" display="flex\" alignItems="center\" justifyContent="center">
         <Spinner size="xl" />
       </Box>
     );
